544316aa9fbb67a656cd1f7e5eba6fd9cdc57b3e
Achim Kraus <achim.kraus@bosch-si.com>
2017-04-25 14:02:35 +0200

    Add trusts for false (not trusting) test.
    Signed-off-by: Achim Kraus <achim.kraus@bosch-si.com>

--- demo-certs/src/main/resources/create-keystores.sh
+++ demo-certs/src/main/resources/create-keystores.sh
@@ -4,6 +4,7 @@ KEY_STORE=keyStore.jks
 KEY_STORE_PWD=endPass
 TRUST_STORE=trustStore.jks
 TRUST_STORE_PWD=rootPass
+NO_TRUST_STORE=noTrustStore.jks
 VALIDITY=365
 
 echo "creating root key and certificate..."
@@ -16,6 +17,10 @@ keytool -genkeypair -alias ca -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=
 keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -certreq -alias ca | \
   keytool -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD -alias root -gencert -validity $VALIDITY -ext BC=0 -rfc | \
   keytool -alias ca -importcert -keystore $TRUST_STORE -storepass $TRUST_STORE_PWD
+
+echo "creating alien root key and certificate for trust failure ..."
+keytool -genkeypair -alias root -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=alien-root' \
+        -ext BC=ca:true -validity $VALIDITY -keypass $TRUST_STORE_PWD -keystore $NO_TRUST_STORE -storepass $TRUST_STORE_PWD
 
 echo "creating server key and certificate..."
 keytool -genkeypair -alias server -keyalg EC -dname 'C=CA,L=Ottawa,O=Eclipse IoT,OU=Californium,CN=cf-server' \
